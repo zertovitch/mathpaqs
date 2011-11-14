272d38c6ae1f2160e1b88fe9b610169e1a301664
gdemont <gdemont@c1092fde-d694-413a-b83e-a663b02e44bd>
2011-11-14 15:49:07 +0000

    Test_samples: a bit more

--- stats/test_samples.adb
+++ stats/test_samples.adb
@@ -9,10 +9,12 @@ procedure Test_Samples is
 
   package RS is new Samples(Real, Quantile_table);
 
-  s: RS.Sample(2000);
+  s: RS.Sample(10_000);
 
   level: constant Quantile_table:=
-   (0.0, 0.25, 0.5, 0.75, 0.8,
+   (0.0, 0.0001, 0.001, 0.01,
+    0.1, 0.2,
+    0.25, 0.5, 0.75, 0.8,
     0.9, 0.95, 0.975, 0.98,
     0.99, 0.995, 0.996, 0.9975, 0.998,
     0.999, 0.9995, 0.9996, 0.99975, 0.9998,
@@ -24,23 +26,49 @@ procedure Test_Samples is
   package RIO is new Ada.Text_IO.Float_IO(Real);
   use RIO;
 
+  procedure Display_Measure(m: RS.Measure) is
+  begin
+    Put("Mean:"); Put(m.mean); New_Line;
+    Put("Std dev:"); Put(m.std_dev); New_Line;
+    Put("Stat err:"); Put(m.stat_err); New_Line;
+    for i in m.level'Range loop
+      Put("P(X<");
+      Put(m.VaR(i));
+      Put(") = ");
+      Put(m.level(i));
+      New_Line;
+    end loop;
+  end Display_Measure;
+
 begin
-  -- Trivial test 1: add only 0 as occurence
+  Put_Line("=== Trivial test 1: add only 0 as occurence");
   RS.Initialize(s, 0.0, 1000.0);
-  for i in 1..10000 loop
+  for i in 1..10_000 loop
     RS.Add_occurence(s, 0.0);
   end loop;
   m.level:= level;
   RS.Get_measures(s,m);
-  Put("Mean, should be zero:"); Put(m.mean); New_Line;
-  Put("Std dev, should be zero:"); Put(m.std_dev); New_Line;
-  -- Trivial test 2: add only 0.123 as occurence
-  RS.Initialize(s, 0.0, 1000.0);
-  for i in 1..10000 loop
-    RS.Add_occurence(s, 0.123);
+  Put_Line("Mean should be zero, std dev should be zero.");
+  Display_Measure(m);
+  --
+  Put_Line("=== Trivial test 2: add only 0.123 as occurence");
+  RS.Initialize(s, 0.0, 1000.0); -- !! narrow it
+  for i in 1..100_000 loop
+    RS.Add_occurence(s, 0.0123);
   end loop;
   m.level:= level;
   RS.Get_measures(s,m);
-  Put("Mean, should be 0.123:"); Put(m.mean); New_Line;
-  Put("Std dev, should be zero:"); Put(m.std_dev); New_Line;
+  Put_Line("Mean should be 0.0123, std dev should be zero.");
+  Display_Measure(m);
+  --
+  Put_Line("=== Easy test 3: discrete uniform, 2 points");
+  RS.Initialize(s, -10.0, 10.0);
+  for i in 1..100_000 loop
+    RS.Add_occurence(s, -10.0);
+    RS.Add_occurence(s,  10.0);
+  end loop;
+  m.level:= level;
+  RS.Get_measures(s,m);
+  Put_Line("Mean should be zero, std dev should be 10.");
+  Display_Measure(m);
 end Test_Samples;
