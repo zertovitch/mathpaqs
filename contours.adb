284eb9da1f41f1b8197b92ae21647b031458a955
gdemont <gdemont@c1092fde-d694-413a-b83e-a663b02e44bd>
2018-12-11 13:21:33 +0000

    More source gardening on the contour plot package

--- contours.adb
+++ contours.adb
@@ -9,22 +9,23 @@ package body Contours is
                     z   : Contour_level)
   is
     h, xh, yh  : array(0..4) of Real;
-    sh : array(0..4) of Integer;
+    sh : array(0..4) of Integer;  --  Sign of relative height.
     im : constant array(1..4) of Integer := (0,1,1,0);
     jm : constant array(1..4) of Integer := (0,0,1,1);
 
     type Case_Value is range 0 .. 9;
-
+    subtype With_Line is Case_Value range 1 .. 9;
+    No_Line : constant := 0;
+    --  Possible cases
     castab : constant array(-1..1, -1..1, -1..1) of Case_Value:=
-
       (( ( 0, 0, 8), (0, 2, 5), (7, 6, 9)),
        ( ( 0, 3, 4), (1, 3, 1), (4, 3, 0)),
        ( ( 9, 6, 7), (5, 2, 0), (8, 0, 0)));
 
     dmin, dmax : Real;
     m1, m2, m3 : Integer;
-    cas : Case_Value;
     x1, y1, x2, y2 : Real;
+    cas : Case_Value;
 
     function xsect(p1,p2: Natural) return Real is
       begin return (h(p2) * xh(p1) - h(p1) * xh(p2)) / (h(p2) - h(p1)); end xsect;
@@ -48,6 +49,7 @@ package body Contours is
           Level_Scan:
           for k in z'Range loop
             if  z ( k ) in dmin .. dmax  then
+              --  Make a local copy of the map within vertices 1,2,3,4; 0 is the centre.
               for m in reverse 0 .. 4 loop
                 if  m > 0  then
                   h(m) := d(i + im(m), j + jm(m)) - z(k);
@@ -58,12 +60,13 @@ package body Contours is
                   xh(0) := 0.5 * (x(i) + x(i + 1));
                   yh(0) := 0.5 * (y(j) + y(j + 1));
                 end if;
+                --  Record the sign of relative height per vertex.
                 if  h ( m ) > 0.0  then
-                  sh(m) := + 1;
+                  sh(m) := +1;
                 elsif  h ( m ) < 0.0  then
                   sh(m) := -1;
                 else
-                  sh(m) := 0;
+                  sh(m) :=  0;
                 end if;
               end loop;
               --
@@ -96,93 +99,92 @@ package body Contours is
               --
               Triangle_Scan:
               for m in 1 .. 4 loop
-                m1 := m;
-                m2 := 0;
-                if  m /= 4  then
-                  m3 := m + 1;
-                else
-                  m3 := 1;
-                end if;
-
-                cas:= castab(sh(m1), sh(m2), sh(m3));
-                if cas /= 0 then
-                  case cas is
-                    when 0 => null;  --  No line (skipped anyway)
-                    --
-                    --     Case 1 - Line between vertices 1 and 2
-                    --
-                    when 1 =>
-                      x1 := xh(m1);
-                      y1 := yh(m1);
-                      x2 := xh(m2);
-                      y2 := yh(m2);
-                    --
-                    --     Case 2 - Line between vertices 2 and 3
-                    --
-                    when 2 =>
-                      x1 := xh(m2);
-                      y1 := yh(m2);
-                      x2 := xh(m3);
-                      y2 := yh(m3);
-                    --
-                    --     Case 3 - Line between vertices 3 and 1
-                    --
-                    when 3 =>
-                      x1 := xh(m3);
-                      y1 := yh(m3);
-                      x2 := xh(m1);
-                      y2 := yh(m1);
-                    --
-                    --     Case 4 - Line between vertex 1 and side 2-3
-                    --
-                    when 4 =>
-                      x1 := xh(m1);
-                      y1 := yh(m1);
-                      x2 := xsect(m2, m3);
-                      y2 := ysect(m2, m3);
-                    --
-                    --     Case 5 - Line between vertex 2 and side 3-1
-                    --
-                    when 5 =>
-                      x1 := xh(m2);
-                      y1 := yh(m2);
-                      x2 := xsect(m3, m1);
-                      y2 := ysect(m3, m1);
-                    --
-                    --     Case 6 - Line between vertex 3 and side 1-2
-                    --
-                    when 6 =>
-                      x1 := xh(m3);
-                      y1 := yh(m3);
-                      x2 := xsect(m1, m2);
-                      y2 := ysect(m1, m2);
-                    --
-                    --     Case 7 - Line between sides 1-2 and 2-3
-                    --
-                    when 7 =>
-                      x1 := xsect(m1, m2);
-                      y1 := ysect(m1, m2);
-                      x2 := xsect(m2, m3);
-                      y2 := ysect(m2, m3);
-                    --
-                    --     Case 8 - Line between sides 2-3 and 3-1
-                    --
-                    when 8 =>
-                      x1 := xsect(m2, m3);
-                      y1 := ysect(m2, m3);
-                      x2 := xsect(m3, m1);
-                      y2 := ysect(m3, m1);
-                    --
-                    --     Case 9 - Line between sides 3-1 and 1-2
-                    --
-                    when 9 =>
-                      x1 := xsect(m3, m1);
-                      y1 := ysect(m3, m1);
-                      x2 := xsect(m1, m2);
-                      y2 := ysect(m1, m2);
-                  end case;
-                  Vecout (x1,y1, x2,y2, z(k));
-                end if;
+                m1 := m;            --  m1 is always the vertex m = 1,2,3,4.
+                m2 := 0;            --  m2 is always the centre, 0.
+                m3 := 1 + m mod 4;  --  m3 is the next vertex after m.
+                --  Get the case corresponding to the sign (-1, 0, or 1)
+                --  attached to each vertex of the triangle.
+                cas := castab(sh(m1), sh(m2), sh(m3));
+                case cas is
+                  when No_Line =>
+                    null;
+                  when With_Line =>
+                    case With_Line(cas) is
+                      --
+                      --     Case 1 - Line between vertices 1 and 2
+                      --
+                      when 1 =>
+                        x1 := xh(m1);
+                        y1 := yh(m1);
+                        x2 := xh(m2);
+                        y2 := yh(m2);
+                      --
+                      --     Case 2 - Line between vertices 2 and 3
+                      --
+                      when 2 =>
+                        x1 := xh(m2);
+                        y1 := yh(m2);
+                        x2 := xh(m3);
+                        y2 := yh(m3);
+                      --
+                      --     Case 3 - Line between vertices 3 and 1
+                      --
+                      when 3 =>
+                        x1 := xh(m3);
+                        y1 := yh(m3);
+                        x2 := xh(m1);
+                        y2 := yh(m1);
+                      --
+                      --     Case 4 - Line between vertex 1 and side 2-3
+                      --
+                      when 4 =>
+                        x1 := xh(m1);
+                        y1 := yh(m1);
+                        x2 := xsect(m2, m3);
+                        y2 := ysect(m2, m3);
+                      --
+                      --     Case 5 - Line between vertex 2 and side 3-1
+                      --
+                      when 5 =>
+                        x1 := xh(m2);
+                        y1 := yh(m2);
+                        x2 := xsect(m3, m1);
+                        y2 := ysect(m3, m1);
+                      --
+                      --     Case 6 - Line between vertex 3 and side 1-2
+                      --
+                      when 6 =>
+                        x1 := xh(m3);
+                        y1 := yh(m3);
+                        x2 := xsect(m1, m2);
+                        y2 := ysect(m1, m2);
+                      --
+                      --     Case 7 - Line between sides 1-2 and 2-3
+                      --
+                      when 7 =>
+                        x1 := xsect(m1, m2);
+                        y1 := ysect(m1, m2);
+                        x2 := xsect(m2, m3);
+                        y2 := ysect(m2, m3);
+                      --
+                      --     Case 8 - Line between sides 2-3 and 3-1
+                      --
+                      when 8 =>
+                        x1 := xsect(m2, m3);
+                        y1 := ysect(m2, m3);
+                        x2 := xsect(m3, m1);
+                        y2 := ysect(m3, m1);
+                      --
+                      --     Case 9 - Line between sides 3-1 and 1-2
+                      --
+                      when 9 =>
+                        x1 := xsect(m3, m1);
+                        y1 := ysect(m3, m1);
+                        x2 := xsect(m1, m2);
+                        y2 := ysect(m1, m2);
+                    end case;
+                    Vecout (x1,y1, x2,y2, z(k));
+                end case;
               end loop Triangle_Scan;
             end if;
           end loop Level_Scan;
