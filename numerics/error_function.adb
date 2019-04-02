229abf361332b5c8620db766a0a9ef64dd78d5d5
gdemont <gdemont@c1092fde-d694-413a-b83e-a663b02e44bd>
2019-04-02 09:10:46 +0000

    Comment & doc gardening

--- numerics/error_function.adb
+++ numerics/error_function.adb
@@ -121,33 +121,4 @@ package body Error_function is
     return S * (1.0 - ErfC(Xa));
   end Erf;
 
-  --  ************************************************************************
-  --  Normal distribution function
-  --
-  --  Returns the area under the Gaussian probability density
-  --  function, integrated from minus infinity to x:
-  --
-  --                           x
-  --                           -
-  --                 1        | |          2
-  --  ndtr(x)  = ---------    |    exp( - t /2 ) dt
-  --             sqrt(2pi)  | |
-  --                         -
-  --                       -inf.
-  --
-  --           =  ( 1 + erf(z) ) / 2
-  --
-  --  where z = x/sqrt(2). Computation is via the functions erf and erfc.
-  --
-  --
-  --  ACCURACY:
-  --
-  --                    Relative error:
-  --  arithmetic   domain     # trials      peak         rms
-  --  IEEE          -13,0        30000      3.4e-14      6.7e-15
-  --
-  --  Cephes Math Library Release 2.8:  June, 2000
-  --  Copyright 1984, 1987, 1988, 1992, 2000 by Stephen L. Moshier
-  --  ************************************************************************
-
 end Error_function;
