44224efcf7826c783d4c79830980455cfc2b70c1
unknown <gdemont@hotmail.com>
2020-12-12 18:25:38 +0100

    Show_floats_limits: more details

--- show_floats_limits.adb
+++ show_floats_limits.adb
@@ -16,6 +16,9 @@ procedure Show_floats_limits is
     Put_Line(name);
     Put_Line("    Largest positive number  . . . . " & F'Image(F'Last));
     Put_Line("    Largest negative number  . . . . " & F'Image(F'First));
+    Put_Line("    Model_Small  . . . . . . . . . . " & F'Image(F'Model_Small));
+    Put_Line("    Epsilon  . . . . . . . . . . . . " & F'Image(F'Epsilon));
+    Put_Line("    Model_Epsilon  . . . . . . . . . " & F'Image(F'Model_Epsilon));
     Put_Line("    Significant decimal digits . . . " & Integer'Image(F'Digits));
     Put_Line("    Storage bits . . . . . . . . . . " & Integer'Image(F'Size));
     Put_Line("    Storage bytes  . . . . . . . . . " & Integer'Image(F'Size / 8));
@@ -23,12 +26,12 @@ procedure Show_floats_limits is
   end Show_limits;
 
   --  In many cases Float is the IEEE single precision type (32 bits, i.e. 4 bytes).
-  --  Single precision accumulates quickly significant rouding errors.
+  --  Single precision accumulates quickly significant rounding errors.
   --  It should to be used only in special cases where memory is scarse.
   --
   procedure SF   is new Show_limits(Float,           "Float");
 
-  --  Often, Long_Float is the IEEE dougle precision type (64 bits, i.e. 8 bytes).
+  --  Often, Long_Float is the IEEE double precision type (64 bits, i.e. 8 bytes).
   --
   procedure SLF  is new Show_limits(Long_Float,      "Long_Float");
 
