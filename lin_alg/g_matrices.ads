202737abc7fc5fa80cf6f8d20ff24622520a1be3
zertovitch <gdemont@hotmail.com>
2023-01-23 00:22:37 +0100

    ObjectAda project file update

--- lin_alg/g_matrices.ads
+++ lin_alg/g_matrices.ads
@@ -2,60 +2,56 @@
 --  File:            gmatrice.ads or g_matrices.ads
 --
 --  Description:     Simple generic matrix package.
---                   The scalars (Field_elt) can be of any kind (real, complex, rationals, ...)
+--                   The scalars (Field_elt) can be of any kind
+--                   (real, complex, rationals, ...)
 --
---                   NB: For Ada 2005+ and Field_elt = floating-point numbers,
---                       it is better, for performance and compatibility reasons,
---                       to use Ada.Numerics.Generic_Real_Arrays instead.
+--  IMPORTANT:
+--  =========
+--            With Ada 2005+ and Field_elt = real or complex (floating-point)
+--            numbers, it is better, for performance and compatibility reasons,
+--            to use the package  Ada.Numerics.Generic_Real_Arrays  instead.
+--
 --
 --  Date / Version:  3-Feb-2005 ; 16-Oct-2001 ; ... ; 22 XII 1997
 --  Author:          Gautier de Montmollin
 ------------------------------------------------------------------------------
 
-generic -- Requires an algebraic field with a square root
-  -- e.g. floating point numbers, fixed point numbers
+generic  --  Requires an algebraic field with a square root
+  --  e.g. floating point numbers, fixed point numbers
 
-  type Field_elt is private; -- Element of the algebraic field
-  zero, one : Field_elt;                       -- 0 and 1 elements
+  type Field_elt is private; --  Element of the algebraic field
+  zero, one : Field_elt;                       --  0 and 1 elements
 
-  with function "-" (a: Field_elt) return Field_elt;    -- unary operator
+  with function "-" (a: Field_elt) return Field_elt;     --  unary operator
   with function Sqrt (a: Field_elt) return Field_elt;
 
-  with function "+" (a, b: Field_elt) return Field_elt;  -- binary operator
+  with function "+" (a, b: Field_elt) return Field_elt;  --  binary operator
   with function "-" (a, b: Field_elt) return Field_elt;
   with function "*" (a, b: Field_elt) return Field_elt;
   with function "/" (a, b: Field_elt) return Field_elt;
   pragma Unreferenced ("/");
-  -- no more "=" for Ada 83 compatibility
+  --  no more "=" for Ada 83 compatibility
 
-  -- Change 2005: Vector, Matrix also as parameter, allows generic on generic
-  -- e.g. G_Matrices in generic FEK's body.
+  --  Change 2005: Vector, Matrix added as parameters.
+  --  Allows generic on generic e.g. G_Matrices in generic FEK's body.
   type Vector is array (Integer range <>) of Field_elt;
   type Matrix is array (Integer range <>, Integer range <>) of Field_elt;
 
 package G_Matrices is
 
-  -- 2005: ghosts of previous versions:
-  -- type IVector is array (Integer range <>) of Integer;
-  -- type IMatrix is array (Integer range <>, Integer range <>) of Integer;
-  -- type p_Matrix is access Matrix;
-  -- type p_Vector is access Vector;
-  -- type p_IMatrix is access IMatrix;
-  -- type p_IVector is access IVector;
-
-  -- Vector operations
+  --  Vector operations
 
   function "*" (l: Field_elt; v: Vector) return Vector;
   function "-" (a: Vector) return Vector;
   function "+" (a, b: Vector) return Vector;
   function "-" (a, b: Vector) return Vector;
-  function "*" (a, b: Vector) return Field_elt;  -- produit scalaire
+  function "*" (a, b: Vector) return Field_elt;       --  produit scalaire
   function Norm (a: Vector) return Field_elt;
-  function Square_norm (a: Vector) return Field_elt;  -- norme au carr\'e
+  function Square_norm (a: Vector) return Field_elt;  --  norme au carr\'e
   function Distance (a, b: Vector) return Field_elt;
   function Square_dist (a, b: Vector) return Field_elt;
 
-  -- Matrix operations
+  --  Matrix operations
 
   function Transpose (A: Matrix) return Matrix;
   function Id (order: Positive) return Matrix;
@@ -64,7 +60,7 @@ package G_Matrices is
   function "+" (A, B: Matrix) return Matrix;
   function "-" (A, B: Matrix) return Matrix;
 
-  -- Matrix-Vector operations
+  --  Matrix-Vector operations
 
   function "*" (A: Matrix; x: Vector) return Vector;
 
